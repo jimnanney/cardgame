e28848258d5420e951c52f4c8d901e34023c52c0
Jim Nanney <jnanney@gmail.com>
2012-12-01 07:48:36 -0600

    Added destroy, edit methods and templates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def new
   end
 
   def show
   end
 
   def create
+
   end
 
-  def delete
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url
   end
 end
