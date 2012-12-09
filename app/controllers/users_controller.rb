48c058b259c71f51bade69bec53ac7b59bd7cc76
Jim Nanney <jnanney@gmail.com>
2012-12-09 07:09:42 -0600

    Add the remaining restful routes to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def create
-
+    @user = User.new(params[:user])
+    @user.save
+    redirect_to @user
   end
 
   def edit
