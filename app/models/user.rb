adf56f627cc994a2b0334b8ebcf1d98f7c5d7bd7
Jim Nanney <jnanney@gmail.com>
2013-02-17 07:46:48 -0600

    Add index to users email, validation tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  before_save { |user| user.email = email.downcase }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,38 @@ describe User do
   
   it { should be_valid }
 
-  describe "When name is not present"
-end
+  describe "When name is not present" do
+    before { @user = User.new(email: "jdoe@example.net") }
+    subject { @user }
+    it { should_not be_valid }
+  end
+  
+  describe "When email is not present" do
+    before { @user = User.new(name: "Joe Doe") }
+    subject { @user }
+    it { should_not be_valid }
+  end
+
+  describe "When two users with same email exist, regardless of case" do
+    before do 
+      @duplicate = User.new(name: "Jim", email: "jim@example.net")
+      @duplicate.save()
+      @user = User.new(name: "Joe", email: "Jim@Example.Net")
+    end
+    subject {@user}
+    it { should_not be_valid }
+  end
+
+  describe "When two users with same email exist in the database" do
+    before do 
+      @duplicate = User.new(name: "Jim", email: "jim@example.net")
+      @duplicate.save()
+      @user = User.new(name: "Joe", email: "Jim@Example.Net")
+      @user.save
+    end
+    subject{ @user }
+    its(:id) { should be_nil }
+  end
+  
+ end
 
